f231e99f4e38181ba5c5b44985b44fa51e6a3d12
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-06-12 16:45:37 +0300

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Password_angular';
-}
+  password: string = '';
+  dynamicClass: string[] = ['gray', 'gray', 'gray'];
+
+  
+
+  passwordStrengthCheck() {
+    this.dynamicClass = ['gray', 'gray', 'gray']
+    if (this.password.length === 0) {
+      this.dynamicClass = ['gray', 'gray', 'gray'];
+    } else if (this.password.length < 8) {
+      this.dynamicClass = ['red', 'red', 'red'];
+    }
+    else if (/[a-zA-Z]/.test(this.password) && /[0-9]/.test(this.password) && /[^\w\s]/.test(this.password)) {
+      this.dynamicClass = ['green', 'green', 'green'];
+    } 
+    else if (/[a-zA-Z]/.test(this.password) && /[0-9]/.test(this.password) || /[a-zA-Z]/.test(this.password) && /[^\w\s]/.test(this.password) || /[0-9]/.test(this.password) && /[^\w\s]/.test(this.password)) {
+      this.dynamicClass = ['yellow', 'yellow', 'gray']
+    } else {
+      this.dynamicClass = ['red', 'gray', 'gray']};
+    }
+    }
+
+  
